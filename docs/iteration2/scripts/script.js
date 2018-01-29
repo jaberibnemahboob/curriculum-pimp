a24fd0d919cd666a8a86b597a36ed5891124d7fa
Jaber Ibne Mahboob <jaber.ibne.mahboob@gmail.com>
2018-01-29 21:12:13 +0100

    cleaning step 1

--- docs/iteration2/scripts/script.js
+++
@@ -1,8 +0,0 @@
-function initialJSEffect(){
-    document.querySelectorAll('*[data-type="label"]').forEach(function(item){
-        let tName = item.getAttribute("data-for");
-        item.addEventListener("click",function(){
-            document.querySelector("#"+tName).checked = (document.querySelector("#"+tName).checked)? false : true;
-        });
-    });
-}
--- docs/wireframes/scripts/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('*[data-type="label"]').forEach(function(item){
-    let tName = item.getAttribute("data-for");
-    item.addEventListener("click",function(){
-        document.querySelector("#"+tName).checked = (document.querySelector("#"+tName).checked)? false : true;
-    });
-});
